915003c316de53da338025bf8a37664180fd7a56
Juan Pablo Pieroni <seisduro@gmail.com>
2025-09-30 12:34:50 -0300

    Capitalize

--- src/utils/serviceWorker.js
+++ src/utils/serviceWorker.js
@@ -1,14 +1,6 @@
-// Service Worker registration
+// Service Worker registration - TEMPORALMENTE DESHABILITADO
 export const registerSW = () => {
-  if ('serviceWorker' in navigator && import.meta.env.PROD) {
-    window.addEventListener('load', () => {
-      navigator.serviceWorker.register('/sw.js')
-        .then((registration) => {
-          console.log('SW registered: ', registration)
-        })
-        .catch((registrationError) => {
-          console.log('SW registration failed: ', registrationError)
-        })
-    })
-  }
+  // Deshabilitado para diagnosticar problema de carga infinita
+  console.log('Service Worker deshabilitado temporalmente')
+  return
 }
